be8f7827c259e0a4649063d0ebe1e63e498267f3
Parul Ghosh <parulghosh2008@gmail.com>
2026-01-03 16:35:24 +0530

    Add program to swap 2 numbers without third variable

---
+++ basics/swap2.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main()
+{
+  int a, b;
+  printf("Enter the numbers:\n");
+  scanf("%d %d", &a, &b);
+  a=a+b;
+  b=a-b;
+  a=a-b;
+  printf("Numbers after swapping are: %d %d", a, b);
+  return 0;
+}
