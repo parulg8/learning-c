58ec77835b0ec92c3a0ec10a4d44127c9b79dd84
Parul Ghosh <parulghosh2008@gmail.com>
2026-01-03 16:54:45 +0530

    Add program to check divisibility by 3 nos. (or)

---
+++ conditions/div2.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main()
+{
+  int a;
+  printf("Enter the number to check divisibility of: ");
+  scanf("%d", &a);
+  if ((a%6==0) || (a%4==0) || (a%2==0))
+  {
+    printf("Number is divisible by 6, 4 or 2.");
+  }
+  else
+  {
+    printf("Number is not divisible by 6, 4 or 2.");
+  }
+  return 0;
+}
