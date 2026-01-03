fe2d09d1d589c0962666c95e4215c2011a5e254b
Parul Ghosh <parulghosh2008@gmail.com>
2026-01-03 16:28:21 +0530

    Add program for a basic arithmetic calculator

---
+++ basics/calc.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main()
+{
+  int a, b;
+  printf("Enter two numbers:");
+  scanf("%d%d", &a, &b);
+  printf("Addition is = %d\n", a+b);
+  printf("Subtraction is = %d\n", a-b);
+  printf("Multiplication is = %d\n", a*b);
+  printf("Division is = %d\n", a/b);
+  return 0;
+}
