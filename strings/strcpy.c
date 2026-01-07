2a890007cf2f33dd6992e8e231d6023dbc7314d1
Parul Ghosh <parulghosh2008@gmail.com>
2026-01-07 16:49:09 +0530

    Add program to copy a string to another

---
+++ strings/strcpy.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <string.h>
+int main()
+{
+  char str1[100], str2[100];
+  printf("Enter string: ");
+  scanf("%[^\n]", str1);
+  strcpy(str2, str1);
+  printf("Copied string is: %s", str2);
+  return 0;
+}
