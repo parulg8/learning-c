1020d798ca0c53774b9591a57751f8c6bebd9a07
Parul Ghosh <parulghosh2008@gmail.com>
2026-01-09 22:09:41 +0530

    Add program for factorial of a number

---
+++ maths/recursion/fact.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+int factorial(int n)
+{
+  if (n==0 || n==1)
+  {
+    return 1;
+  }
+  else
+  {
+    return n * factorial(n-1);
+  }
+}
+int main()
+{
+  int n;
+  printf("Enter a number: ");
+  scanf("%d", &n);
+  if (n < 0)
+  {
+    printf("Factorial not defined.\n");
+  }
+  else
+  {
+    printf("Factorial of %d is %d\n", n, factorial(n));
+  }
+  return 0;
+}
